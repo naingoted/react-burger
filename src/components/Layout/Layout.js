b20b2b526682a8dceab95a4c2386df9a2f601e98
vincent <thetno@sph.com.sg>
2018-02-01 22:40:15 +0800

    135

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css'
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar , SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-        
-    </Aux>
-);
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    SideDrawerToggleHandler = () => {
+        // this.setState({showSideDrawer: !this.state.showSideDrawer}); - async will create random outcome
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer};
+        
+      });
+    }
+    render() {
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}  />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+    
+}
+export default Layout;
